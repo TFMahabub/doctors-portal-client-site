2832fddad05d130326cbe72fb880a560ab1ab6f6
TFMahabub <rjmahabub543@gmail.com>
2022-11-14 10:58:07 +0600

    modal open done

--- src/Components/Page/Appointment/BottomSection/Carts.js
+++ src/Components/Page/Appointment/BottomSection/Carts.js
@@ -1,22 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import SingleCart from './SingleCart';
+import React, { useEffect, useState } from "react";
+import BookingModal from "./BookingModal";
+import SingleCart from "./SingleCart";
 
 const Carts = () => {
-  const [appointments, setAppointments]=useState([])
-  
+  const [appointments, setAppointments] = useState([]);
+  const [modalTitle, setModalTitle] = useState('');
 
-  useEffect(()=>{
-    fetch('AppoinmentOptions.json')
-    .then(res=>res.json())
-    .then(data=>setAppointments(data))
-  },[])
+  useEffect(() => {
+    fetch("AppoinmentOptions.json")
+      .then((res) => res.json())
+      .then((data) => setAppointments(data));
+  }, []);
   return (
-    <div className='container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-16'>
-      {
-        appointments.map(ap=><SingleCart key={ap._id} ap = {ap}></SingleCart>)
-      }
-
-
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-16">
+      {appointments.map((ap) => (
+        <SingleCart 
+        key={ap._id} 
+        ap={ap}
+        setModalTitle={setModalTitle}
+        />
+      ))}
+      <BookingModal
+       appointments = {appointments}
+       modalTitle={modalTitle}
+      ></BookingModal>
     </div>
   );
 };
--- src/Components/Page/Appointment/BottomSection/SingleCart.js
+++ src/Components/Page/Appointment/BottomSection/SingleCart.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
 
-const SingleCart = ({ap}) => {
-  const {name, slots} = ap;
+const SingleCart = ({ ap, setModalTitle }) => {
+  const { name, slots } = ap;
   return (
-    <div className='border p-8 text-center shadow-lg'>
-      <h3 className='text-xl font-semibold text-secondary'>{name}</h3>
-      <p className='tracking-wide'>{slots.length > 0 ? slots[0] : 'Try Another Day'}</p>
-      <p className='tracking-wide'>{slots.length > 1 ? `${slots.length}  SPACESES AVAILABLE` : `${slots.length} SPACES AVAILABLE`}</p>
-      <button className="btn mt-3 px-6 tracking-wide bg-gradient-to-r from-primary to-secondary text-[#fff] text-md border-0">Book Appointment</button>
+    <div className="border p-8 text-center shadow-lg">
+      <h3 className="text-xl font-semibold text-secondary">{name}</h3>
+      <p className="tracking-wide">
+        {slots.length > 0 ? slots[0] : "Try Another Day"}
+      </p>
+      <p className="tracking-wide">
+        {slots.length > 1
+          ? `${slots.length}  SPACESES AVAILABLE`
+          : `${slots.length} SPACES AVAILABLE`}
+      </p>
+      <button htmlFor="booking-modal" onClick={() => setModalTitle(name)}>
+        <label
+          htmlFor="booking-modal"
+          className="btn mt-3 px-6 tracking-wide bg-gradient-to-r from-primary to-secondary text-[#fff] text-md border-0"
+        >
+          Book Appointment
+        </label>
+      </button>
     </div>
   );
 };
